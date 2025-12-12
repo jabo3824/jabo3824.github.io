921221b9f207c72d51bc18bb304cc92020f4d178
jabo3824 <jabo3824@colorado.edu>
2025-12-11 23:57:28 -0500

    Update globe.js

--- js/globe.js
+++ js/globe.js
@@ -137,20 +137,26 @@ function onClick() {
     if (intersects.length > 0) {
         const clickedMarker = intersects[0].object;
         const location = clickedMarker.userData.location;
-        animateToLocation(location);
+        animateToLocation(location, clickedMarker);
         showLocationInfo(location);
     } else {
         closeLocationInfo();
     }
 }
 
-function animateToLocation(location) {
-    const lat = location.lat;
-    const lon = location.lon;
+function animateToLocation(location, marker) {
+    // Get the current world position of the marker
+    const markerWorldPos = new THREE.Vector3();
+    marker.getWorldPosition(markerWorldPos);
     
-    // Rotate to center the location
-    targetRotation.x = -lat * Math.PI / 180;
-    targetRotation.y = -lon * Math.PI / 180;
+    // Calculate the angles needed to rotate this point to face the camera
+    // Camera is looking down the -Z axis, so we want the point at (0, 0, -distance)
+    const currentY = Math.atan2(markerWorldPos.x, markerWorldPos.z);
+    const currentX = Math.asin(markerWorldPos.y / 1.02);
+    
+    // Calculate target rotation (rotate from current position to center)
+    targetRotation.y = globe.rotation.y - currentY;
+    targetRotation.x = globe.rotation.x - currentX;
     
     isAnimatingToLocation = true;
     rotationVelocity = { x: 0, y: 0 };
